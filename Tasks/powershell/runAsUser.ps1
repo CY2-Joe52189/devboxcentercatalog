e342eb00090c521e4eac8c6ee00b6789e8798023
CY2-Joe52189 <joe52189@cy2.co.uk>
2024-11-12 11:23:29 +0000

    Corrections to pwsh run as user

--- Tasks/powershell/runAsUser.ps1
+++ Tasks/powershell/runAsUser.ps1
@@ -1,7 +1,8 @@
 $CustomizationScriptsDir = "C:\DevBoxCustomizations"
 $LockFile = "lockfile"
 
-Write-Host "Staring the script as user"
+Write-Host "Starting the script as user"
+Write-Host "DO NOT CLOSE THIS WINDOW"
 Start-Sleep -Seconds 3
 
 Remove-Item -Path "$($CustomizationScriptsDir)\$($LockFile)" -Force
